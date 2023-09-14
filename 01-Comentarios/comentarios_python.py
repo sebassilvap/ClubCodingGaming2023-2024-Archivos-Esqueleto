505ff0e73ce944482cb0a3c0f85940fec2588b50
Sebastian Silva <sebas.silva.p@gmail.com>
2023-09-14 15:40:32 +0200

    Actualización 14.09.2023

---
+++ 01-Comentarios/comentarios_python.py
@@ -0,0 +1,32 @@
+# ===================================================
+# Comentarios en Python
+
+# - Notas / Puntos Importantes
+# - SÚPER IMPORTANTES PARA INICIAR
+# - Comentarios de una línea
+# - No afectan la ejecución del código
+# De esta manera se ponen comentarios en Python!
+# ===================================================
+
+# Extensión de VS Code => Better Comments
+# Esto funciona dentro del IDE de VS Code
+
+
+
+
+
+
+# ========================
+# Comentarios Multilínea
+# ========================
+# Por defecto no existen comentarios multilínea en Python
+# Podemos utilizar una notación de cadena de texto especial para este fin
+# Que funcionan como comentarios
+
+
+
+
+
+# TEST:
+# Run Code
+# => COMANDO: CTRL + ALT + N
